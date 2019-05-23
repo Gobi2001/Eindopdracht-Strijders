196ef8e3f76d6606a4c6b0c5dc01dfbfb9f2b668
Gobi2001 <43144280+Gobi2001@users.noreply.github.com>
2019-05-23 09:56:54 +0200

    Update login.php

--- Inloggen.Registreren/login.php
+++ Inloggen.Registreren/login.php
@@ -24,13 +24,7 @@ if (isset($_POST['btnInloggen']))
         echo "<script>alert(\"Login Failed\");</script>";
     }
 }
-
-
 ?>
-
-
-
-
 
 <html>
 <head>
@@ -104,9 +98,7 @@ if (isset($_POST['btnInloggen']))
 </div>
 <!--------------------------------------------------------------------------------------------------------------------->
 
-
-
-<!-- Footer -->
+<!-- Footer------------------------------------------------------------------------------------------------------------>
 <footer class="py-5 bg-dark">
     <div class="container" style="text-align: center">
         <br>
@@ -125,12 +117,8 @@ if (isset($_POST['btnInloggen']))
         <br>
         <p class="m-0 text-center text-white">Copyright &copy; Eindopdracht Strijders 2019</p>
     </div>
-    <!-- /.container -->
+    <!--container------------------------------------------------------------------------------------------------------>
 </footer>
-
-
-
-
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
